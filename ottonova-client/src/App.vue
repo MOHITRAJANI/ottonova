b16110bac3568105adaca586217e3343fa274cbe
Mohit Rajani <smartmonu00798@gmail.com>
2023-08-11 20:03:47 +0200

    feat!: add working carousel component

--- ottonova-client/src/App.vue
+++ ottonova-client/src/App.vue
@@ -1,25 +1,92 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Carousel :items-to-show="1">
+    <Slide v-for="slide in 10" :key="slide">
+      {{ slide }}
+    </Slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </Carousel>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.carousel__prev {
+  border: 2px solid white;
+}
+
+.carousel__next {
+  border: 2px solid white;
+}
+
+.carousel__icon {
+  color: white;
+}
+
+.backimage {
+  width: 100%;
+  height: 100%;
+}
+
+.carousel {
+  height: 100vh;
+}
+
+.carousel__slide {
+  padding: 5px;
+}
+
+.carousel__viewport {
+  perspective: 2000px;
+  background-color: whitesmoke;
+  height: 95%;
+  display: flex;
+}
+
+.carousel__track {
+  transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+  transition: 0.5s;
+}
+
+.carousel__slide {
+  opacity: 0.9;
+  transform: rotateY(-20deg) scale(0.9);
+}
+
+.carousel__slide--active ~ .carousel__slide {
+  transform: rotateY(20deg) scale(0.9);
+}
+
+.carousel__slide--prev {
+  opacity: 1;
+  transform: rotateY(-10deg) scale(0.95);
+}
+
+.carousel__slide--next {
+  opacity: 1;
+  transform: rotateY(10deg) scale(0.95);
+}
+
+.carousel__slide--active {
+  opacity: 1;
+  transform: rotateY(0) scale(1.1);
 }
 </style>
--- ottonova-client/src/main.js
+++ ottonova-client/src/main.js
@@ -1,4 +1,10 @@
+// basic setup
 import { createApp } from 'vue'
 import App from './App.vue'
+
+
+// CSS imports
+import 'vue3-carousel/dist/carousel.css' // Carousel
+
 
 createApp(App).mount('#app')
